cf5eeb92fd5ea034961387a642f3ae3d56155427
Théo Lévêque <theo.leveque@gmail.com>
2024-11-04 09:36:52 -0800

    chore: colors and font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      black: '#232323',
+      white: '#FFFFFF',
+      'dark-grey': '#474747',
+      grey: '#8483A0',
+      tan: '#D6C099',
+      'tan-light': '#EAE8D0',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 }
