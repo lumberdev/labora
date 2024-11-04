2cfcb976ddfac0c9f5a38edf36ef19fef39814cf
Théo Lévêque <theo.leveque@gmail.com>
2024-11-04 10:34:38 -0800

    feat: mobile nav stub

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+type Props = {
+  anchors: string[]
+  children: React.ReactNode
+}
+
+const MobileNav = ({ anchors, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}>{children}</button>
+      <div className={`${isOpen ? 'block' : 'hidden'}`}>
+        {anchors.map((anchor) => (
+          <a href={`#${anchor}`}>{anchor}</a>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default MobileNav
