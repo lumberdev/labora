749bbbd6a0b686bfde52a6713b2bad5bda3f5b46
Théo Lévêque <70675996+theolvq@users.noreply.github.com>
2024-11-13 14:53:16 -0800

    theo/feat/cityscale animation (#2)
    * chore: add react-spring
    
    * feat: basic horizontal scroll animation
    
    * chore: update prettier config
    
    * chore: better cloud asset
    
    * feat: horizontal animation

---
+++ src/components/HorizontalAnimation.tsx
@@ -0,0 +1,49 @@
+import { animated, useSpring } from '@react-spring/web'
+import React from 'react'
+
+type Props = {
+  to: number
+  from: number
+  duration?: number
+  precision?: number
+  loop?: boolean
+  reset?: boolean
+  children: React.ReactNode
+}
+
+const HorizontalAnimation = ({
+  to,
+  from,
+  duration = 30000,
+  precision = 0.0001,
+  loop = true,
+  reset = true,
+  children,
+}: Props) => {
+  const [styles] = useSpring(
+    {
+      from: { x: from },
+      to: async (next) => {
+        await next({ x: to })
+        await next({ x: from, immediate: true })
+      },
+      config: {
+        duration,
+        precision,
+      },
+      loop,
+      reset,
+    },
+    [],
+  )
+  return (
+    <animated.div
+      className="flex h-full w-max"
+      style={{ transform: styles.x.to((x) => `translateX(${x}%)`) }}
+    >
+      {children}
+    </animated.div>
+  )
+}
+
+export default HorizontalAnimation
