b900f6154d3dc3ceed8b160734704a88d4fcdfc0
Théo Lévêque <theo.leveque@gmail.com>
2024-11-15 15:11:04 -0800

    chore: svg support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import svgr from 'vite-plugin-svgr'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [
+      svgr({
+        include: '**/*.svg?react',
+        svgrOptions: {
+          plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+          svgoConfig: {
+            plugins: [
+              'preset-default',
+              'removeTitle',
+              'removeDesc',
+              'removeDoctype',
+              'cleanupIds',
+            ],
+          },
+        },
+      }),
+    ],
+  },
+})
