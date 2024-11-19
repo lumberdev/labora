435b11165f36f7c88118448f67b0f44461c947ab
Théo Lévêque <theo.leveque@gmail.com>
2024-11-19 15:20:11 -0800

    style: padding, font size and order

--- src/components/CityscapeScroll.tsx
+++ src/components/CityscapeScroll.tsx
@@ -4,14 +4,14 @@ import HorizontalAnimation from './HorizontalAnimation'
 
 const CityscapeScroll = () => {
   return (
-    <div className="relative h-[462px] w-full overflow-hidden">
-      <div className="absolute top-0 h-[232px]">
+    <div className="relative h-[250px] w-full overflow-hidden md:h-[462px]">
+      <div className="absolute top-0 h-[150px] md:h-[232px]">
         <HorizontalAnimation to={0} from={-50}>
           <img src={clouds.src} alt="Clouds" />
           <img src={clouds.src} alt="Clouds" />
         </HorizontalAnimation>
       </div>
-      <div className="absolute bottom-0 h-[434px]">
+      <div className="absolute bottom-0 h-[250px] md:h-[434px]">
         <HorizontalAnimation to={-50} from={0}>
           <img src={cityscapeDesktop.src} alt="Cityscape" />
           <img src={cityscapeDesktop.src} alt="Cityscape" />
