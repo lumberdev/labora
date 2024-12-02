d4e8c7615938cd8d586e3ddf828a02b19b5bb84e
Théo Lévêque <theo.leveque@gmail.com>
2024-12-02 15:56:51 -0800

    refactor: location selector to select companies

--- src/components/LocationSelector.tsx
+++ src/components/LocationSelector.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { Globe } from './globe/Globe'
-import { locations, type CountryKey } from '@/lib/data'
+import { locations, type Company } from '@/lib/data'
 
 const LocationSelector = () => {
-  const [selectedLocation, setSelectedLocation] = useState<CountryKey | null>(
-    null,
+  const [selectedCompany, setSelectedCompany] = useState<Company>(
+    locations[0].companies[0],
   )
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const company = locations.find((l) => e.target.name.includes(l.label))
+    setSelectedCompany(
+      company?.companies.find((c) => c.name === e.target.value)!,
+    )
+  }
   return (
     <div className="grid lg:min-h-[610px] lg:grid-cols-2">
       <div className="order-2 h-full w-full overflow-auto p-6 md:order-1">
@@ -16,34 +22,29 @@ const LocationSelector = () => {
               <h3 className="text-lg font-semibold">{location.label}</h3>
               {location.companies.map((company) => (
                 <div key={company.name} className="space-y-2 pl-4">
-                  <h4 className="text-md font-medium">{company.name}</h4>
-                  <div className="space-y-2 pl-4">
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name={`${location.label}-${company.name}`}
+                      value={company.name}
+                      checked={selectedCompany.name === company.name}
+                      onChange={handleChange}
+                      className="form-radio"
+                    />
+                    <h4 className="text-md font-medium">{company.name}</h4>
+                  </label>
+                  <ul className="space-y-2 pl-4">
                     {company.countries.map((country) => (
-                      <label
-                        key={country}
-                        className="flex cursor-pointer items-center space-x-2"
-                      >
-                        <input
-                          type="radio"
-                          name="location"
-                          value={country}
-                          checked={selectedLocation === country}
-                          onChange={(e) =>
-                            setSelectedLocation(e.target.value as CountryKey)
-                          }
-                          className="form-radio"
-                        />
-                        <span>{country}</span>
-                      </label>
+                      <li key={country.id}>{country.name}</li>
                     ))}
-                  </div>
+                  </ul>
                 </div>
               ))}
             </div>
           ))}
         </div>
       </div>
-      <Globe selectedLocation={selectedLocation} />
+      <Globe selectedLocations={selectedCompany.countries} />
     </div>
   )
 }
